87c167aefa77efb2e39dea8e352aa8995b763e9a
Ali <alistair.mackinnon@outlook.com>
2019-11-25 20:34:34 +0000

    initial commit of wallpaper changes script

---
+++ wallpaper-changer.py
@@ -0,0 +1,22 @@
+import os
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+
+# set urls
+url = 'https://apod.nasa.gov/apod/astropix.html'
+img_base_url = 'https://apod.nasa.gov/'
+
+# get and parse files
+resp = requests.get(url)
+html = BeautifulSoup(resp.text, featulsres="html.parser")
+img = html.find('img')
+img = img['src']
+img_link = os.path.join(img_base_url, img)
+
+# download image
+urllib.request.urlretrieve(img_link, '/home/ali/Pictures/potd.jpg')
+
+# refresh wallpaper
+os.system('xfdesktop --reload')
+
